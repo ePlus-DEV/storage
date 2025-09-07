6853fe413cab052678715ed07aab7788b544dfea
David Nguyen <h250694@gmail.com>
2025-09-07 14:33:50 +0700

    Securing Virtual Machines using Chrome Enterprise Premium - GSP1036

---
+++ labs/GSP1036/README.md
@@ -0,0 +1,3 @@
+# Securing Virtual Machines using Chrome Enterprise Premium - GSP1036
+
+<https://eplus.dev/securing-virtual-machines-using-chrome-enterprise-premium-gsp1036>
--- labs/GSP1036/lab.sh
+++ labs/GSP1036/lab.sh
@@ -1,10 +1,29 @@
+#!/bin/bash
+# Define color variables
 
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
-# Set text styles
-YELLOW=$(tput setaf 3)
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
-BOLD=$(tput bold)
-RESET=$(tput sgr0)
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution - ePlus.DEV ${RESET}"
 
 export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
@@ -41,3 +60,8 @@ echo "${YELLOW}${BOLD}$PROJECT_NUMBER-compute@developer.gserviceaccount.com${RES
 echo ""
 
 echo ""
+
+
+echo "${BG_RED}${BOLD}Congratulations For Completing!!! - ePlus.DEV ${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
