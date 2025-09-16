bf822738f6800fa8a12e4eba24084a587bc2c6a0
David Nguyen <h250694@gmail.com>
2025-09-16 11:35:53 +0700

    Introduction to Cloud Dataproc: Hadoop and Spark on Google Cloud - GSP123

--- labs/GSP123/lab.sh
+++ labs/GSP123/lab.sh
@@ -25,25 +25,80 @@ RESET=`tput sgr0`
 
 echo "${BG_MAGENTA}${BOLD}Starting Execution - ePlus.DEV ${RESET}"
 
-cat > request.json <<EOF_END
-{
-    "config": {
-        "encoding":"FLAC",
-        "languageCode": "en-US"
-    },
-    "audio": {
-        "uri":"gs://cloud-samples-tests/speech/brooklyn.flac"
-    }
-  }
-EOF_END
+echo "Please set the below values correctly"
+read -p "${YELLOW}${BOLD}Enter the CLUSTER_NAME: ${RESET}" CLUSTER_NAME
+
+# Export variables after collecting input
+export CLUSTER_NAME 
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
 
-curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
-"https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}"
+PROJECT_NUMBER=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
+    --role="roles/storage.admin"
+
+sleep 60
+
+#!/bin/bash
+
+echo "$CLUSTER_NAME"
+echo "$ZONE"
+echo "$REGION"
+
+cluster_function() {
+  gcloud dataproc clusters create "$CLUSTER_NAME" \
+    --region "$REGION" \
+    --zone "$ZONE" \
+    --master-machine-type n1-standard-2 \
+    --worker-machine-type n1-standard-2 \
+    --num-workers 2 \
+    --worker-boot-disk-size 100 \
+    --worker-boot-disk-type pd-standard \
+    --no-address
+}
+
+cp_success=false
+
+while [ "$cp_success" = false ]; do
+  cluster_function
+  exit_status=$?
+
+  if [ "$exit_status" -eq 0 ]; then
+    echo "Function deployed successfully [https://www.youtube.com/@techcps]"
+    cp_success=true
+  else
+    echo "Cluster creation failed. Checking if cluster already exists..."
+
+    if gcloud dataproc clusters describe "$CLUSTER_NAME" --region "$REGION" &>/dev/null; then
+      echo "Cluster already exists. Deleting. [https://www.youtube.com/@techcps]"
+      gcloud dataproc clusters delete "$CLUSTER_NAME" --region "$REGION" --quiet
+      echo "Cluster deleted. Retrying in 10 seconds..."
+    else
+      echo "Unknown errors. Retrying in 10 seconds[https://www.youtube.com/@techcps]"
+    fi
+    echo "Please subscribe to techcps [https://www.youtube.com/@techcps]"
+    sleep 10
+  fi
+done
 
 
-curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
-"https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
+gcloud dataproc jobs submit spark \
+    --project $DEVSHELL_PROJECT_ID \
+    --region $REGION \
+    --cluster $CLUSTER_NAME \
+    --class org.apache.spark.examples.SparkPi \
+    --jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
+    -- 1000
+
+
+echo "Click this link to open" "${YELLOW}${BOLD}https://console.cloud.google.com/dataproc/jobs?project=$DEVSHELL_PROJECT_ID${RESET}"
 
 echo "${BG_RED}${BOLD}Congratulations For Completing!!! - ePlus.DEV ${RESET}"
 
