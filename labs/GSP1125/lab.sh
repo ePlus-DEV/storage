88204c008bbbcfa50fa1ff20df694f72f0ac0943
David Nguyen <h250694@gmail.com>
2025-09-20 18:49:16 +0700

    Detect and Investigate Threats with Security Command Center - GSP1125

--- labs/GSP1125/lab.sh
+++ labs/GSP1125/lab.sh
@@ -34,7 +34,7 @@ RANDOM_BG_COLOR=${BG_COLORS[$RANDOM % ${#BG_COLORS[@]}]}
 
 #----------------------------------------------------start--------------------------------------------------#
 
-echo "${RANDOM_BG_COLOR}${RANDOM_TEXT_COLOR}${BOLD}Starting Execution${RESET}"
+echo "${RANDOM_BG_COLOR}${RANDOM_TEXT_COLOR}${BOLD}Starting Execution - ePlus.DEV${RESET}"
 
 # Step 1: Get Compute Zone & Region
 echo "${YELLOW}${BOLD}Fetching Compute Zone & Region...${RESET}"
