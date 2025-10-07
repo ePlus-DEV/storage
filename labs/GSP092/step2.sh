448848774519ad6c13267b3308af7dc84ca29b71
David Nguyen <h250694@gmail.com>
2025-10-07 13:40:09 +0700

    Monitoring and Logging for Cloud Run Functions - GSP092

--- labs/GSP092/step2.sh
+++ labs/GSP092/step2.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution - ePlus.DEV ${RESET}"
+
+
 curl -LO 'https://github.com/tsenart/vegeta/releases/download/v12.12.0/vegeta_12.12.0_linux_386.tar.gz'
 
 tar -xvzf vegeta_12.12.0_linux_386.tar.gz
@@ -6,3 +34,7 @@ gcloud logging metrics create CloudFunctionLatency-Logs \
     --project=$DEVSHELL_PROJECT_ID \
     --description="Subscribe to Arcade Helper" \
     --log-filter='resource.type="cloud_run_revision" AND resource.labels.function_name="helloWorld"'
+
+echo "${BG_RED}${BOLD}Congratulations For Completing!!! - ePlus.DEV ${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
