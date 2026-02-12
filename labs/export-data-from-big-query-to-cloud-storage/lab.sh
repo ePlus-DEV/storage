c91daab76e09e033a2d8c20dfb4853801cb6c080
David Nguyen <h250694@gmail.com>
2026-02-12 08:48:19 +0700

    Export Data from BigQuery to Cloud Storage

--- labs/export-data-from-big-query-to-cloud-storage/lab.sh
+++ labs/export-data-from-big-query-to-cloud-storage/lab.sh
@@ -1,5 +1,16 @@
+# ========== COLORS ==========
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+BOLD='\033[1m'
+# ============================
 
-
+echo -e "${BLUE}${BOLD}▶ Starting... - ePlus.DEV${RESET}"
 
 
 
@@ -602,5 +613,4 @@ SELECT * FROM \`bracketology.ncaa_2019_tournament\`
 
 
 
-
-echo "🎉 DONE! - ePlus.DEV"
+echo -e "${RED}${BOLD}▶ 🎉 DONE! - ePlus.DEV${RESET}"
